6c95f136f06f008fe707731ac59b4ac8a9897d1d
Zeno Zeng <zenoofzeng@gmail.com>
2014-06-12 21:31:13 +0800

    add display_student_courses/add_student_course/remove_student_course/set_student_course_score for AdminView, fixes #13

--- views/admin.cpp
+++ views/admin.cpp
@@ -4,17 +4,25 @@ using namespace std;
 
 void AdminView::help () {
     cout << "Type following commands for further step:" << endl;
+    // common
+    cout << "	exit: logout" << endl;
     cout << "	help: get help" << endl;
+    // course
     cout << "	courses: list all courses" << endl;
     cout << "	course: show detail of course" << endl;
     cout << "	edit course: edit course" << endl;
     cout << "	add course: add course" << endl;
     cout << "	remove course: remove course" << endl;
+    // user
     cout << "	users: list all users" << endl;
     cout << "	edit user: edit user" << endl;
     cout << "	add user: add user" << endl;
     cout << "	remove user: remove user" << endl;
-    cout << "	exit: logout" << endl;
+    // student course
+    cout << "	courses of student: list all courses of a student" << endl;
+    cout << "	add course for student: add course for student" << endl;
+    cout << "	remove course for student: remove course for student" << endl;
+    cout << "	set course score for student: set course score for student" << endl;
 }
 
 void AdminView::dispatch (string cmd) {
@@ -34,6 +42,14 @@ void AdminView::dispatch (string cmd) {
         this->remove_user();
     } else if ( cmd == "edit course" || cmd == "add course" ) {
         this->edit_course();
+    } else if ( cmd == "courses of student" ) {
+        this->display_student_courses();
+    } else if ( cmd == "add course for student" ) {
+        this->add_student_course();
+    } else if ( cmd == "remove course for student" ) {
+        this->remove_student_course();
+    } else if ( cmd == "set course score for student" ) {
+        this->set_student_course_score();
     } else {
         cout << "Invalid Command!" << endl;
     }
@@ -88,4 +104,40 @@ void AdminView::remove_course () {
     cout << "ID: ";
     cin >> id;
     this->course_manager.remove(id);
+}
+
+void AdminView::display_student_courses () {
+    string user_id;
+    cout << "User ID: ";
+    cin >> user_id;
+    this->student_course_manager.list(user_id);
+}
+
+void AdminView::add_student_course () {
+    string user_id, course_id;
+    cout << "User ID: ";
+    cin >> user_id;
+    cout << "Course ID: ";
+    cin >> course_id;
+    this->student_course_manager.add(user_id, course_id);
+}
+
+void AdminView::remove_student_course () {
+    string user_id, course_id;
+    cout << "User ID: ";
+    cin >> user_id;
+    cout << "Course ID: ";
+    cin >> course_id;
+    this->student_course_manager.remove(user_id, course_id);
+}
+
+void AdminView::set_student_course_score () {
+    string user_id, course_id, score;
+    cout << "User ID: ";
+    cin >> user_id;
+    cout << "Course ID: ";
+    cin >> course_id;
+    cout << "Score: ";
+    cin >> score;
+    this->student_course_manager.set(user_id, course_id, "score", score);
 }
--- views/admin.hpp
+++ views/admin.hpp
@@ -2,8 +2,12 @@
 #define JW_ADMIN_VIEW_H
 
 #include "base.hpp"
+#include "../models/student_course_manager.hpp"
 
 class AdminView :public BaseView{
+
+private:
+    StudentCourseManager student_course_manager;
 
 public:
     void help();
@@ -13,5 +17,9 @@ public:
     void edit_course();
     void remove_user();
     void remove_course();
+    void display_student_courses();
+    void add_student_course();
+    void remove_student_course();
+    void set_student_course_score();
 };
 #endif
